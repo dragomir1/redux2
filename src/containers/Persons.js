75ab5f5b6f30bbadf3117ffa256015429ce0aab3
Michael Dragomir <michaeldragomir@mac.com>
2018-05-30 10:56:00 -0700

    additons

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,44 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
+import * as actionTypes from '../store/actions';
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
-
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
-
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
-
     render () {
         return (
             <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
-                    <Person 
+                <AddPerson personAdded={this.props.onAddedPerson} />
+                {this.props.prs.map(person => (
+                    <Person
                         key={person.id}
-                        name={person.name} 
-                        age={person.age} 
-                        clicked={() => this.personDeletedHandler(person.id)}/>
+                        name={person.name}
+                        age={person.age}
+                        clicked={() => this.props.onRemovePerson(person.id)}/>
                 ))}
             </div>
         );
     }
 }
+// access to state. this recieves state as an input passed in the function automatically by redux.
+// it should return a js object where we map a slice of redux state to something we'll be able to access as a property in our container component
+const mapStateToProps = state => {
+  return {
+    prs: state.persons
+  };
+};
 
-export default Persons;
+// this you access to dispatch functions and being able to dispatch. recives the dispatch function as an input and returns an object where we map props to dispatch function executions.
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddedPerson: () => dispatch({type: actionTypes.ADD_PERSON}),
+    onRemovePerson: (id) => dispatch({type: actionTypes.REMOVE_PERSON, personId: id})
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
